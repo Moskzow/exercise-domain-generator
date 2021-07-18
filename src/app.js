870556905fcfcfe34d825b11ebf2c8699d69f143
Moskzow <inmoskwetrust@gmail.com>
2021-07-18 22:52:18 +0000

    Done

--- src/app.js
+++ src/app.js
@@ -9,3 +9,21 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["the", "your", "a"];
+let adj = ["sleepy", "big", "sad", "hungry", "crazy"];
+let noun = ["jogger", "racoon", "pirate", "loco", "detective"];
+let domain = [".com", ".es", ".org", ".io", ".net"];
+
+const Generator = () => {
+  for (let a = 0; a < pronoun.length; a++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        for (let d = 0; d < domain.length; d++) {
+          console.log(pronoun[a] + adj[b] + noun[c] + domain[d]);
+        }
+      }
+    }
+  }
+};
+Generator();
